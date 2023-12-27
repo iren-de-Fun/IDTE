8863121fe93e3ba72b8d3fe4856f577aa8aaca7d
iren-de-Fun <154063530+iren-de-Fun@users.noreply.github.com>
2023-12-27 16:17:29 +0200

    Update 1010_code.js

--- 1010_code.js
+++ 1010_code.js
@@ -1,59 +1,60 @@
-AFRAME.registerComponent("registerevents", {
-  init: function () {
-    var marker = this.el;
-    marker.addEventListener('markerFound', function () {
-      console.log('Знайдено маркер', marker.id);
-      marker_visible[marker.id] = true;
-    });
-    marker.addEventListener('markerLost', function () {
-      console.log('Втрачено маркер', marker.id);
-      marker_visible[marker.id] = false;
-    });
-  }
-});
+let mnarker_visible={ A: false, B: false};
 
-AFRAME.registerComponent("run", {
-  init: function () {
-    this.A = document.querySelector("a-marker#A");
-    this.B = document.querySelector("a-marker#B");
-    this.C = document.querySelector("a-marker#C");
-    this.D = document.querySelector("a-marker#D");
+AFRAME.registerComponent("registerevents"
+        {
+	            init: function () {
+                         var marker = this.el;
+                         marker.addEventListener('markerFound', function() {
+                                     console.log('Знайдено маркер', marker.id); 
+                                     marker_visible [marker.id] =true; 
+                                 });
+                         marker.addEventListener('markerLost', function() {
+                                  console.log('Втрачено маркер', marker.id); 
+                                  marker_visible [marker.id] =false;                  
+                                 });
+                         },                  
+           {
+);
 
-    this.createLine(this.A, this.B, "lineAB");
-    this.createLine(this.B, this.C, "lineBC");
-    this.createLine(this.C, this.D, "lineCD");
-    this.createLine(this.D, this.A, "lineDA");
-  },
-  createLine: function (point1, point2, lineName) {
-    const line = document.createElement('a-entity');
-    line.setAttribute('line', { start: '0 0 0', end: '0 0 0', color: 'red' });
-    point1.appendChild(line);
-    this[lineName] = line;
-  },
-  tick: function (time, deltaTime) {
-    if (marker_visible["A"] && marker_visible["B"] && marker_visible["C"] && marker_visible["D"]) {
-      console.log("A, B, C, and D");
+AFRAME.registerComponent("run", 
+                         {
+                         	init: function () {
+                                   this.A = document.querySelector ("a-marker#A");
+                                   this.B = document.querySelector ("a-marker#B");
+                                   this.AB = document.querySelector ("#AB").object3D;
 
-      const lines = ["lineAB", "lineBC", "lineCD", "lineDA"];
+                                   const geometry = new THREE.CylinderGeometry( 0.05, 0.05, 1, 32 );
+                                   geometry.applyMatrix4 (new THREE.Matrix4().makeTranslation (0, 0.5, 0));
+                                   geometry.applyMatrix4 (new THREE.Matrix4().makeRotationX (Math.PI/2));
 
-      for (const lineName of lines) {
-        const line = this[lineName];
-        const startPoint = line.parentElement.object3D.position;
-        const endPoint = line.parentElement.components[lineName.slice(4)].object3D.position;
+                                   const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+                                   this.lineAB = new THREE.Mesh( geometry, material );
+                                   
 
-        line.setAttribute('line', {
-          start: `${startPoint.x} ${startPoint.y} ${startPoint.z}`,
-          end: `${endPoint.x} ${endPoint.y} ${endPoint.z}`,
-          color: 'red'
-        });
-      }
-    } else {
-      // Сховати лінії, якщо не всі точки видимі
-      const lines = ["lineAB", "lineBC", "lineCD", "lineDA"];
-      lines.forEach(lineName => {
-        const line = this[lineName];
-        line.setAttribute('line', { start: '0 0 0', end: '0 0 0', color: 'red' });
-      });
-    }
-  }
-});
+
+                                   this.lineAB.visible=false; 
+                                   
+                                   this.AB.add (this.lineAB); 
+                                   console.log (this.AB);
+                         	},
+                                   tick: function (time, deltaTime) {
+                                             if (marker_visible["A"] && mnarker_visible ["B"])
+                                                console.log("A and B");
+                                                this.lineAB.visible=true;
+
+                                                const vecA = new THREE.Vector3();
+                                                const vecB = new THREE.Vector3();
+                                                this.A.object3D.getworldPosstion(vecA);
+                                                this.B.object3D.getworldPosstion(vecB);
+                                                const vidstanAB = vecA.distanceTo(vecB);
+                                                this.lineAB.lookAt(vecB);
+                                                this.lineAB.scale.set(1,1,vidstanAB);
+                                                //console.log(vecA);
+                                                //console.log(vecB);
+                                                console.log("AB = ",vidstanAB); 
+
+                                   }
+                                            if(!marker_visible["A"] || !marker_visible["В"])
+                                            this.lineAB.visible=false;
+          }
+}); 
