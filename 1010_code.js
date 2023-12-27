663aaf963c3901088bc49382d38e6c264afd55d7
iren-de-Fun <154063530+iren-de-Fun@users.noreply.github.com>
2023-12-27 17:04:56 +0200

    Update 1010_code.js

--- 1010_code.js
+++ 1010_code.js
@@ -21,12 +21,6 @@ AFRAME.registerComponent("run", {
     this.B = document.querySelector("a-marker#B");
     this.C = document.querySelector("a-marker#C");
     this.D = document.querySelector("a-marker#D");
-    this.AB = document.querySelector("#pointA").object3D;
-
-    const geometry = new THREE.CylinderGeometry(0.05, 0.05, 1, 32);
-        geometry.applyMatrix4(new THREE.Matrix4().makeTranslation(0, 0.5, 0));
-        geometry.applyMatrix4(new THREE.Matrix4().makeRotationX(Math.PI / 2));
-        const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 
     // Створення точок на сцені
     pointA = document.createElement('a-entity');
@@ -39,73 +33,50 @@ AFRAME.registerComponent("run", {
     this.C.appendChild(pointC);
     this.D.appendChild(pointD);
 
-    // Створення лінії
-    const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
-    this.lineAB = new THREE.Line(new THREE.BufferGeometry(), material);
-    this.lineBC = new THREE.Line(new THREE.BufferGeometry(), material);
-    this.lineCD = new THREE.Line(new THREE.BufferGeometry(), material);
-    this.lineDA = new THREE.Line(new THREE.BufferGeometry(), material);
-
-    this.AB.add(this.lineAB);
-    this.BC.add(this.lineAB);
-    this.CD.add(this.lineAB);
-    this.DA.add(this.lineAB);
+    // Створення ліній
+    this.createLine(this.A, this.B, "lineAB");
+    this.createLine(this.B, this.C, "lineBC");
+    this.createLine(this.C, this.D, "lineCD");
+    this.createLine(this.D, this.A, "lineDA");
 
     // Позначення координат у консолі
-    console.log('Point A:', pointA.object3D.position);
-    console.log('Point B:', pointB.object3D.position);
-    console.log('Point C:', pointC.object3D.position);
-    console.log('Point D:', pointD.object3D.position);
+    console.log('Point A:', this.A.object3D.position);
+    console.log('Point B:', this.B.object3D.position);
+    console.log('Point C:', this.C.object3D.position);
+    console.log('Point D:', this.D.object3D.position);
   },
- tick: function (time, deltaTime) {
-        if (marker_visible["A"] && marker_visible["B"] && marker_visible["C"] && marker_visible["D"]) {
-            this.lineAB.visible = true;
-            this.lineBC.visible = true;
-            this.lineCD.visible = true;
-            this.lineDA.visible = true;
+  createLine: function (point1, point2, lineName) {
+    const line = document.createElement('a-entity');
+    line.setAttribute('line', { color: 'red' });
+    point1.appendChild(line);
+    this[lineName] = line;
+  },
+  tick: function (time, deltaTime) {
+    if (marker_visible["A"] && marker_visible["B"] && marker_visible["C"] && marker_visible["D"]) {
+      console.log("A, B, C, and D");
 
-            const vecA = new THREE.Vector3();
-            const vecB = new THREE.Vector3();
-            const vecC = new THREE.Vector3();
-            const vecD = new THREE.Vector3();
+      const lines = ["lineAB", "lineBC", "lineCD", "lineDA"];
 
-            this.A.object3D.getWorldPosition(vecA);
-            this.B.object3D.getWorldPosition(vecB);
-            this.C.object3D.getWorldPosition(vecC);
-            this.D.object3D.getWorldPosition(vecD);
-            const vidstanAB = vecA.distanceTo(vecB);
-            const vidstanBC = vecB.distanceTo(vecC);
-            const vidstanCD = vecC.distanceTo(vecD);
-            const vidstanDA = vecD.distanceTo(vecA);
-            this.lineAB.lookAt(vecB);
-            this.lineAB.scale.set(1, 1, vidstanAB);
+      for (let i = 0; i < lines.length; i++) {
+        const lineName = lines[i];
+        const line = this[lineName];
+        const startPoint = line.parentElement.object3D.position;
+        const nextPointName = lines[(i + 1) % lines.length];
+        const endPoint = this[nextPointName].object3D.position;
 
-            this.lineBC.lookAt(vecC);
-            this.lineBC.scale.set(1, 1, vidstanBC);
-
-            this.lineCD.lookAt(vecD);
-            this.lineCD.scale.set(1, 1, vidstanCD);
-
-            this.lineDA.lookAt(vecA);
-            this.lineDA.scale.set(1, 1, vidstanDA);
-
-            console.log("AB = ", vidstanAB);
-            console.log("BC = ", vidstanBC);
-            console.log("CD = ", vidstanCD);
-            console.log("DA = ", vidstanDA);
-      });
-
-      // Створення лінії
-      this.lineAB.geometry.dispose();
-      this.lineAB.geometry = new THREE.BufferGeometry().setFromPoints(positions.concat(positions[0]));
-
-      // Додаткова інформація у консолі
-      console.log('Point A:', pointA.object3D.position);
-      console.log('Point B:', pointB.object3D.position);
-      console.log('Point C:', pointC.object3D.position);
-      console.log('Point D:', pointD.object3D.position);
+        line.setAttribute('line', {
+          start: `${startPoint.x} ${startPoint.y} ${startPoint.z}`,
+          end: `${endPoint.x} ${endPoint.y} ${endPoint.z}`,
+          color: 'red'
+        });
+      }
     } else {
-      this.lineAB.geometry.dispose();
+      // Сховати лінії, якщо не всі точки видимі
+      const lines = ["lineAB", "lineBC", "lineCD", "lineDA"];
+      lines.forEach(lineName => {
+        const line = this[lineName];
+        line.setAttribute('line', { color: 'red' });
+      });
     }
   }
 });
