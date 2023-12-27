2784bd53e1b2b619c3be106e897819f73732c5a2
iren-de-Fun <154063530+iren-de-Fun@users.noreply.github.com>
2023-12-27 16:13:21 +0200

    Update 1010_code.js

--- 1010_code.js
+++ 1010_code.js
@@ -1,17 +1,3 @@
-AFRAME.registerComponent("registerevents", {
-  init: function () {
-    var marker = this.el;
-    marker.addEventListener('markerFound', function () {
-      console.log('Знайдено маркер', marker.id);
-      marker_visible[marker.id] = true;
-    });
-    marker.addEventListener('markerLost', function () {
-      console.log('Втрачено маркер', marker.id);
-      marker_visible[marker.id] = false;
-    });
-  }
-});
-
 AFRAME.registerComponent("run", {
   init: function () {
     this.A = document.querySelector("a-marker#A");
@@ -19,41 +5,31 @@ AFRAME.registerComponent("run", {
     this.C = document.querySelector("a-marker#C");
     this.D = document.querySelector("a-marker#D");
 
-    this.createLine(this.A, this.B, "lineAB");
-    this.createLine(this.B, this.C, "lineBC");
-    this.createLine(this.C, this.D, "lineCD");
-    this.createLine(this.D, this.A, "lineDA");
+    this.createLine("lineABCD");
   },
-  createLine: function (point1, point2, lineName) {
+  createLine: function (lineName) {
     const line = document.createElement('a-entity');
     line.setAttribute('line', { start: '0 0 0', end: '0 0 0', color: 'red' });
-    point1.appendChild(line);
+    this.A.appendChild(line);
     this[lineName] = line;
   },
   tick: function (time, deltaTime) {
     if (marker_visible["A"] && marker_visible["B"] && marker_visible["C"] && marker_visible["D"]) {
       console.log("A, B, C, and D");
 
-      const lines = ["lineAB", "lineBC", "lineCD", "lineDA"];
+      const line = this["lineABCD"];
+      const startPoint = this.A.object3D.position;
+      const endPoint = this.D.object3D.position;
 
-      for (const lineName of lines) {
-        const line = this[lineName];
-        const startPoint = line.parentElement.object3D.position;
-        const endPoint = line.parentElement.components[lineName.slice(4)].object3D.position;
-
-        line.setAttribute('line', {
-          start: `${startPoint.x} ${startPoint.y} ${startPoint.z}`,
-          end: `${endPoint.x} ${endPoint.y} ${endPoint.z}`,
-          color: 'red'
-        });
-      }
-    } else {
-      // Сховати лінії, якщо не всі точки видимі
-      const lines = ["lineAB", "lineBC", "lineCD", "lineDA"];
-      lines.forEach(lineName => {
-        const line = this[lineName];
-        line.setAttribute('line', { start: '0 0 0', end: '0 0 0', color: 'red' });
+      line.setAttribute('line', {
+        start: `${startPoint.x} ${startPoint.y} ${startPoint.z}`,
+        end: `${endPoint.x} ${endPoint.y} ${endPoint.z}`,
+        color: 'red'
       });
+    } else {
+      // Сховати лінію, якщо не всі точки видимі
+      const line = this["lineABCD"];
+      line.setAttribute('line', { start: '0 0 0', end: '0 0 0', color: 'red' });
     }
   }
 });
